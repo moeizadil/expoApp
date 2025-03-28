f84443cdfc00c9f1a4f7732e13702f563bdac036
moeizadil <moeizadil786@gmail.com>
2025-03-29 00:16:26 +0100

    hello world

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View,Text } from "react-native";
+
+const Index = () => {
+  return <View style= {{backgroundColor:"red",
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+  }}>
+    <Text style={{fontSize:30 , fontWeight:"700"}}>"Hello Welcome to React Native Expo"</Text>
+  </View>
+}
+export default Index;
